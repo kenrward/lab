2cdf6836122a9258e9bb9c3e8492211efa5b54ab
Ken Ward <kenrward@gmail.com>
2025-11-06 15:13:36 -0500

    refactor

--- modules/zn_seg_server/main.tf
+++ modules/zn_seg_server/main.tf
@@ -1,43 +1,19 @@
-# --- Data sources ---
-data "vsphere_datacenter" "dc" {
-  name = var.vsphere_datacenter
-}
 
-data "vsphere_datastore" "datastore" {
-  name          = var.vsphere_datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
+########################################
+# Create Windows Member Server VM
+########################################
 
-data "vsphere_network" "network" {
-  name          = var.vsphere_network
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Look up the ESXi host and get its resource pool
-data "vsphere_host" "esxi" {
-  name          = var.vsphere_host
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "template" {
-  name          = var.template_name
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-
-locals {
-  resource_pool_id = coalesce(var.resource_pool_id, data.vsphere_host.esxi.resource_pool_id)
-}
-# --- Seg Server VM ---
 resource "vsphere_virtual_machine" "seg" {
   name             = var.vm_name
-  resource_pool_id = local.resource_pool_id
+  resource_pool_id = local.resource_pool_id 
   datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = var.cores
-  memory           = var.memory_mb
-  guest_id         = "windows2019srv_64Guest"
+  folder           = var.folder
+
+  num_cpus  = var.cores
+  memory    = var.memory_mb
+  guest_id  = "windows2019srv_64Guest"
+  scsi_type = "lsilogic-sas"
   firmware         = "efi"
-  scsi_type        = "lsilogic-sas"
 
   network_interface {
     network_id   = data.vsphere_network.network.id
@@ -47,40 +23,57 @@ resource "vsphere_virtual_machine" "seg" {
   disk {
     label            = "${var.vm_name}.vmdk"
     size             = var.disk_size_gb
+    eagerly_scrub    = false
     thin_provisioned = true
   }
 
   clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    customize {
-      windows_options {
-        computer_name  = var.vm_name
-        admin_password = var.admin_password
-        time_zone      = 85
-      }
-      network_interface {}
-      ipv4_gateway = var.gateway
+  template_uuid = data.vsphere_virtual_machine.template.id
+
+  customize {
+    windows_options {
+      computer_name  = var.vm_name
+      admin_password = var.admin_password
+      time_zone      = 035  
     }
+
+    network_interface {}
   }
+}
+
+
+  ########################################
+  # Cloudbase-Init (Userdata Injection)
+  ########################################
 
   extra_config = {
-    "guestinfo.metadata" = base64encode(templatefile("${path.module}/metadata.yaml", { hostname = var.vm_name }))
-    "guestinfo.userdata" = base64encode(templatefile("${path.module}/userdata-seg.tpl", {
-      HOSTNAME        = var.vm_name
-      DOMAIN_FQDN     = var.domain_fqdn
-      JOIN_USERNAME   = var.join_username
-      JOIN_PASSWORD   = var.join_password
-      ADMIN_PASSWORD  = var.admin_password
-      DC_IP           = var.dc_ip
-      INSTALL_SCRIPT  = var.install_script
-      READY_URL       = var.ready_check_url
-      READY_PORT      = var.ready_check_port
-      READY_PATH      = var.ready_check_path
+      "guestinfo.metadata"           = base64encode(templatefile("${path.module}/metadata.yaml", {
+      hostname = var.vm_name
     }))
-    "guestinfo.metadata.encoding" = "base64"
+     "guestinfo.userdata"           = base64encode(templatefile("${path.module}/userdata-seg.tpl", {
+      HOSTNAME       = var.vm_name
+      DOMAIN_FQDN    = var.domain_fqdn
+      NETBIOS_NAME   = var.netbios_name
+      READY_URL      = var.ready_check_url
+      READY_PORT     = var.ready_check_port
+      READY_PATH     = var.ready_check_path
+      ADMIN_PASSWORD = var.admin_password
+      JOIN_USERNAME  = var.join_username
+      JOIN_PASSWORD  = var.join_password
+      DC_IP          = var.dc_ip
+      SEGTOKEN        = var.segtoken
+    }))
     "guestinfo.userdata.encoding" = "base64"
   }
-
-  wait_for_guest_net_timeout = 300
-  wait_for_guest_net_routable = true
+  # Enable VMware Tools interaction (needed for guestinfo)
+  tools_upgrade_policy = "manual"
+  wait_for_guest_net_timeout = 10
+  
+  # Allow Terraform to destroy and recreate when template changes
+  lifecycle {
+    ignore_changes = [
+      clone[0].template_uuid
+    ]
+  }
 }
+
--- modules/zn_seg_server/variables.tf
+++ modules/zn_seg_server/variables.tf
@@ -1,47 +1,104 @@
-variable "vm_name" {}
-variable "vsphere_datacenter" {}
-variable "vsphere_datastore" {}
-variable "vsphere_network" {}
-variable "resource_pool_id" {
-  description = "Optional resource pool ID override"
-  type        = string
-  default     = null
+variable "vm_name" {
+  type = string
 }
-variable "template_name" {}
-variable "domain_fqdn" {}
-variable "join_username" {}
-variable "join_password" {}
-variable "admin_password" {}
-variable "install_script" {
-  description = "Optional path or command to install the SegServer app"
-  default     = "C:\\Installers\\SegSetup.exe /quiet /norestart"
+
+variable "cores" {
+  type    = number
+  default = 4
+}
+
+variable "memory_mb" {
+  type    = number
+  default = 8192
+}
+
+variable "domain_fqdn" {
+  type = string
+}
+
+variable "netbios_name" {
+  type = string
 }
 
 variable "ready_check_url" {
-  description = "HTTP endpoint exposed by the domain controller when the domain is ready"
-  type        = string
+  type = string
 }
 
 variable "ready_check_port" {
-  description = "TCP port used for the readiness endpoint"
-  type        = number
+  type = number
 }
 
 variable "ready_check_path" {
-  description = "HTTP path appended to the readiness endpoint"
-  type        = string
+  type = string
 }
-variable "gateway" {}
-variable "disk_size_gb" { default = 150 }
-variable "cores" { default = 2 }
-variable "memory_mb" { default = 4096 }
 
-variable "vsphere_host" {
-  description = "ESXi host name for resource pool lookup"
+variable "join_username" {
+  type = string
+}
+
+variable "join_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "disk_size_gb" {
+  type    = number
+  default = 100
 }
 
 variable "dc_ip" {
-  description = "IP address of the domain controller for DNS and domain join"
+  description = "IP address of the domain controller"
+  type        = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+# --- vSphere Environment ---
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter name"
+  type        = string
+}
+
+variable "vsphere_cluster" {
+  description = "vSphere cluster name"
+  type        = string
+}
+
+variable "vsphere_network" {
+  description = "vSphere network name"
+  type        = string
+}
+
+variable "vsphere_datastore" {
+  description = "vSphere datastore name"
+  type        = string
+}
+
+variable "template_name" {
+  description = "vSphere template name to clone from"
+  type        = string
+}
+
+variable "folder" {
+  description = "vSphere folder to place VM in (optional)"
+  type        = string
+  default     = ""
+}
+variable "gateway" {
+  description = "Default IPv4 gateway for the member server"
+  type        = string
+}
+
+variable "vsphere_host" {
+  description = "Name or IP of the ESXi host where the VM will be created"
+  type        = string
+}
+
+variable "segtoken" {
+  description = "Segmentation server Install token"
   type        = string
 }
 
